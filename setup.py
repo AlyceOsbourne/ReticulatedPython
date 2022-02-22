dcdc45490382cd862dffa321f2997659e51006ab
Alyce <Alyceosbourne@outlook.com>
2022-02-22 18:10:41 +0000

    test

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='SR',
+    version='',
+    packages=['common_modules'],
+    url='',
+    license='',
+    author='Alyce',
+    author_email='',
+    description=''
+)

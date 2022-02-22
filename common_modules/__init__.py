6c676d1bf5bedae3cddc8513a9269467cc52ced5
Alyce <Alyceosbourne@outlook.com>
2022-02-22 20:32:57 +0000

    test

--- common_modules/__init__.py
+++ common_modules/__init__.py
@@ -1,8 +1,11 @@
+from functools import partial
+
 from common_modules.__neural_network_tools__ import *
 import tqdm, rich, rich.console as _rc_
 
 console = rich.console.Console()
-print = console.print # replacing print with console print, this is to keep it uniform, adds hints etc
+print = partial(console.print,
+                highlight=True)  # replacing print with console print, this is to keep it uniform, adds hints etc
 log = console.log
 
 __all__ = [*(i for i in dir() if not i.startswith("_") and not i.endswith("_"))]  # will let me load * cleanly
